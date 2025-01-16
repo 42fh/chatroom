8c322f08245f76caac975bd3c2cac695ea1c6e3e
linseed-pivot04@icloud.com <linseed-pivot04@icloud.com>
2025-01-16 12:19:44 +0100

    make the test file location and c file location independent

--- tests/compilation_test.sh
+++ tests/compilation_test.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
-cc -Wall -Werror -Wextra ../srcs/main.c -o compilation_test.out && \
+echo $1
+
+cc -Wall -Werror -Wextra $1 -o compilation_test.out && \
 	rm compilation_test.out && echo OK || echo Error
