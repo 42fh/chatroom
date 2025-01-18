c4826c032730673152d79ca09d5e0aa4a51a77aa
linseed-pivot04@icloud.com <linseed-pivot04@icloud.com>
2025-01-18 20:50:19 +0100

    feature: error message on failing socket note that there is no test to test this!

--- srcs/main.c
+++ srcs/main.c
@@ -1,11 +1,34 @@
+#include <unistd.h>
 #include <sys/select.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <stdlib.h>
 
-#define NULL 0
+
+
+void put_string_fd(const char *str, int fd)
+{
+	write(fd, str, strlen(str));
+}
+
+void exit_with_message(int status, const char *message)
+{
+	put_string_fd(message, 2);
+	exit(status);
+}
 
 int main(int ac, char **av)
 {
-	(void) av;
+	int server_socket;
 	(void) ac;
+	(void) av;
+		
+	server_socket = socket(PF_INET, SOCK_STREAM, 0);
+	if (server_socket < 0)
+		exit_with_message(1, "Failed to create socket");
+			
+
+	
 	struct timeval timeout;
 	timeout.tv_sec = 1;
 	timeout.tv_usec = 1;
